14e2da7a69a02f4afcf31b77f5dcc6d1149308b2
Maeve Crossan <maevecrossan@gmail.com>
2024-09-11 16:30:02 +0000

    Add spread operator examples.

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,29 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4)    // This will result in both arrays being altered 
+console.log("Second array:", arr2);
+console.log("First array:", arr1);
 
 // Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3]; // Spreads the values of the 3rd array into the 4th, creating an entirely new object and leaving the original array untouched.
+
+arr4.push(7);
+console.log("Third array:", arr3);
+console.log("Fourth array:", arr4)
 
 // Copying an object
+let obj1 = { a:1, b:2, c:3};
+let obj2 = { ...obj1, d:4}; // copying and adding on another value without altering original.
+let obj3 = {...obj1, b:5};
+
+console.log("First object:", obj1);
+console.log("Second object:", obj2);
+console.log("Third object:", obj3);
 
 // Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+console.log("Fifth array:", arr5);
